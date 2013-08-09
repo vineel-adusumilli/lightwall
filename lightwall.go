a17a2f9f97b7f9d8d09c63ffe3d4c57d0face92b
Vineel Adusumilli <vineel@adusumilli.org>
2013-08-09 16:22:39 -0700

    Now supports dynamic serial ports and templates.

--- lightwall.go
+++ lightwall.go
@@ -3,47 +3,99 @@ package main
 import (
   "fmt"
   "strconv"
+  "strings"
   "net/http"
+  "html/template"
   "io"
+  "io/ioutil"
   "github.com/tarm/goserial"
 )
 
 var s io.ReadWriteCloser
-var red, green, blue int
 
-func index(w http.ResponseWriter, r *http.Request) {
-  if (r.Method == "POST") {
-    var err error
-    red, err = strconv.Atoi(r.FormValue("r"))
-    if err != nil {
-      red = 0
-    }
-    green, err = strconv.Atoi(r.FormValue("g"))
-    if err != nil {
-      green = 0
-    }
-    blue, err = strconv.Atoi(r.FormValue("b"))
-    if err != nil {
-      blue = 0
-    }
-    s.Write([]byte{byte(red), byte(green), byte(blue), 1})
+func loadSerial() {
+  // find the serial port the Arduino is on. It's usually /dev/ttyACM*
+  files, err := ioutil.ReadDir("/dev")
+  if err != nil {
+    panic("Unable to list /dev!")
   }
-  fmt.Fprintf(w, "<p>lightwall</p>\n")
-  fmt.Fprintf(w, "<form action=\"/\" method=\"post\">")
-  fmt.Fprintf(w, "r: <input type=\"text\" name=\"r\" value=\"%v\" />", red)
-  fmt.Fprintf(w, "g: <input type=\"text\" name=\"g\" value=\"%v\" />", green)
-  fmt.Fprintf(w, "b: <input type=\"text\" name=\"b\" value=\"%v\" />", blue)
-  fmt.Fprintf(w, "<input type=\"submit\" name=\"Submit\" />")
-  fmt.Fprintf(w, "</form>")
-}
+  port := ""
+  for _, f := range files {
+    if strings.HasPrefix(f.Name(), "ttyACM") {
+      port = f.Name()
+      break
+    }
+  }
+  if port == "" {
+    panic("Arduino not connected!")
+  }
 
-func main() {
-  c := &serial.Config{Name: "/dev/ttyACM0", Baud: 115200}
-  var err error
+  c := &serial.Config{Name: "/dev/" + port, Baud: 115200}
   s, err = serial.OpenPort(c)
   if err != nil {
     panic("Unable to open serial port!");
   }
+  fmt.Printf("Successfully connected to Arduino on /dev/%s.\n", port)
+}
+
+var templates *template.Template
+
+func loadTemplates() {
+  // find all the files in templates/ ending in .html
+  files, err := ioutil.ReadDir("templates/")
+  if err != nil {
+    panic("Unable to list templates/!")
+  }
+  templateFiles := make([]string, 0)
+  for _, f := range files {
+    if strings.HasSuffix(f.Name(), ".html") {
+      templateFiles = append(templateFiles, "templates/" + f.Name())
+    }
+  }
+
+  // actually load the templates
+  templates = template.Must(template.ParseFiles(templateFiles...))
+
+  for _, t := range templateFiles {
+    fmt.Printf("Loaded %s.\n", t)
+  }
+}
+
+type Color struct {
+  R, G, B int
+}
+
+var color Color
+
+func index(w http.ResponseWriter, r *http.Request) {
+  if (r.Method == "POST") {
+    var err error
+    color.R, err = strconv.Atoi(r.FormValue("r"))
+    if err != nil {
+      color.R = 0
+    }
+    color.G, err = strconv.Atoi(r.FormValue("g"))
+    if err != nil {
+      color.G = 0
+    }
+    color.B, err = strconv.Atoi(r.FormValue("b"))
+    if err != nil {
+      color.B = 0
+    }
+    s.Write([]byte{byte(color.R), byte(color.G), byte(color.B), 1})
+  }
+
+  err := templates.ExecuteTemplate(w, "index.html", color)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }
+}
+
+func main() {
+  loadSerial()
+  loadTemplates()
+
+  fmt.Println("Opening server on localhost:8080")
   http.HandleFunc("/", index)
   http.ListenAndServe(":8080", nil)
 }
